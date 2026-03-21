6d13c83474df4f0dece4a75bf2f7673a0733b59b
Krimskiu2004 <157579888+Krimskiu2004@users.noreply.github.com>
2026-03-21 11:00:30 +0300

    Day 8: Material Design styling for buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,6 @@ dependencies {
     // Для viewModels delegation
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.android.material:material:1.11.0")
 
 }
